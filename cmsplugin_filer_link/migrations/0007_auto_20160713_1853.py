52755b6bf8bc37075370e49564b8b36620657e90
David Mills <d.mills@guesny.net>
2023-09-03 13:04:29 +0200

    Fixing on_delete in migrations

--- cmsplugin_filer_link/migrations/0007_auto_20160713_1853.py
+++ cmsplugin_filer_link/migrations/0007_auto_20160713_1853.py
@@ -7,13 +7,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cmsplugin_filer_link', '0006_auto_20160705_1334'),
+        ('cmsplugin_filer_file', '0004_auto_20160705_1334'),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='filerlinkplugin',
+            model_name='filerfile',
             name='cmsplugin_ptr',
-            field=models.OneToOneField(parent_link=True, related_name='cmsplugin_filer_link_filerlinkplugin', primary_key=True, serialize=False, to='cms.CMSPlugin'),
+            field=models.OneToOneField(parent_link=True, related_name='cmsplugin_filer_file_filerfile', primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE),
         ),
     ]
